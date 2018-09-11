d96c7bb148dab29a14778099b95cceb31fa8ed89
Pavel Belov <pavel.belov@kofax.com>
2018-09-11 17:16:43 +0300

    removed tokens

--- src/com/psbelov/java/tg/bot/Tokens.java
+++ src/com/psbelov/java/tg/bot/Tokens.java
@@ -4,7 +4,7 @@ public class Tokens {
     //This simply takes the bots API key from the first command line argument sent to the bot.
     //You do not have to retrieve the API key in this way.
 
-    public static String API_KEY = "110806101:AAEp3HsQO55wOvl8w8rH8r1MeeM3UVnCKYs";
-    public static String YANDEX_KEY = "03.163120769:b75817dd0100c89bb46411df888551c0";
-    public static String ALPHAVANTAGE_KEY = "R41N3ZFCZ7SOZ5J3";
+    public static String API_KEY = "";
+    public static String YANDEX_KEY = "";
+    public static String ALPHAVANTAGE_KEY = "";
 }
